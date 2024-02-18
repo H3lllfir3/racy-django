2b112e805f266ec836ffc7d5f3dc7abec447223e
H3lllfir3 <gholami.ayub73+github@gmail.com>
2024-02-19 00:04:12 +0100

    script fixed to issue SSL certificate

--- get_cert.sh
+++ get_cert.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+read -p "Enter domains (example.com www.example.com): " domains  
+read -p "Enter an email: " email
+
+domain_args=""
+
+for domain in $domains; do
+  if [ -z "$domain_args" ]; then
+    domain_args="$domain" 
+  else  
+    domain_args="$domain_args, $domain"
+  fi
+done
+
+
 conf_cert() {
 
     mkdir -p /etc/letsencrypt
@@ -11,14 +25,30 @@ conf_cert() {
 # Use a 4096 bit RSA key instead of 2048.
 rsa-key-size = 4096
 # Set email and domains.
-email = admin@example.com
-domains = example.com, www.example.com
+email = $email
+domains = $domain_args
 # Text interface.
 text = True
 # Suppress the Terms of Service agreement interaction.
 agree-tos = True
 EOF
     certbot certonly --standalone
+
+    for domain in $domains; do
+
+        privkey="/etc/letsencrypt/live/$domain/privkey.pem"
+        cert="/etc/letsencrypt/live/$domain/cert.pem"  
+
+        if [ -f $privkey ] && [ -f $cert ]; then
+            echo "Found keys for $domain"  
+            cp $privkey ./project/privkey.pem
+            cp $cert ./project/cert.pem 
+        else
+            echo "No Let's Encrypt keys found for $domain"
+        fi
+
+    done
+
 }
 
 if command -v certbot &> /dev/null
@@ -26,7 +56,7 @@ then
     conf_cert
     
 else 
-    echo "You don't have certbot installed."
+    echo "Installing certbot..."
     sudo apt install certbot -y
     conf_cert
 fi
